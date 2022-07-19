c0ec38490d80466c4bda5367a26acd9e376a4c97
Vivek Sehrawat <>
2022-07-19 21:33:12 +0530

    intial setup of Xcode

---
+++ TGAFoodDelivery/Pods/Target Support Files/Pods-TGAFoodDeliveryTests/Pods-TGAFoodDeliveryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TGAFoodDeliveryTests : NSObject
+@end
+@implementation PodsDummy_Pods_TGAFoodDeliveryTests
+@end
---
+++ TGAFoodDelivery/TGAFoodDelivery/TGAFoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TGAFoodDeliveryApp.swift
+//  TGAFoodDelivery
+//
+//  Created by Vivek Sehrawat on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TGAFoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
