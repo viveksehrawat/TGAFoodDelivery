b3eb03831c5d53091c8899de1b1f27c07a4c56c5
Vivek Sehrawat <>
2022-07-19 22:35:13 +0530

    added UI of Login page

--- TGAFoodDelivery/TGAFoodDelivery/ContentView.swift
+++ TGAFoodDelivery/TGAFoodDelivery/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = ""
+    @State var pass = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+               
+                VStack{
+                    TextField("Enter your Email address", text: $email)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                       
+                    SecureField("Enter your password", text: $pass)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Sign In")
+                            .frame(width: 200, height: 40)
+                            .foregroundColor(Color.white)
+                            .background(Color.blue)
+                    }).padding()
+                       
+                }
+                .padding()
+                
+                Spacer()
+            }
+            .navigationTitle("Sign In")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+            ContentView()
+                .preferredColorScheme(.light)
+          
+        }
     }
 }
